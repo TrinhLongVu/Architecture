ddea59c6432decd8ba2fb44b3af9375cb73b7df0
sinful_johnny <megaVNN321123@gmail.com>
2024-07-21 18:16:29 +0700

    complete implementation of stat boxes

--- src/frontend-web/src/pages/AdminDashboard/AdminDashboard.jsx
+++ src/frontend-web/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import "./AdminDashboard.css"
+import barcharticon from "../../assets/bar-chart-icon.png"
+import note_icon from "../../assets/note-icon.png"
+import tag_icon from "../../assets/tag-icon.png"
+import StatBox from '../../../components/StatBox'
 
 function AdminDashboard() {
   return (
-    <div>AdminDashboard</div>
+    <div>
+      <div className="stats-banner">
+        <h2>Stats</h2>
+        <div className="stats-container">
+          <StatBox 
+            icon={barcharticon}
+            statNumber={100}
+            statTitle="Total Players"
+            statSubtitle="+8% from yesterday"
+            backgroundColor="#FCE9EC"
+          />
+
+          <StatBox 
+            icon={note_icon}
+            statNumber={20}
+            statTitle="Total Brands"
+            statSubtitle="+5% from yesterday"
+            backgroundColor="#FEF3E6"
+          />
+
+          <StatBox 
+            icon={tag_icon}
+            statNumber={10}
+            statTitle="Games Created"
+            statSubtitle="+8% from yesterday"
+            backgroundColor="#E9FAEF"
+          />
+        </div>
+      </div>
+    </div>
+      
+    
+    
   )
 }
 
