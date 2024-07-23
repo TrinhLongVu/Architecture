6e1aa10a869c01cf767a2e658a6229315a46006b
sinful_johnny <megaVNN321123@gmail.com>
2024-07-23 13:18:14 +0700

    Add charts to admin dashboard

--- src/frontend-web/components/StatBox.jsx
+++ src/frontend-web/components/StatBox.jsx
@@ -9,14 +9,14 @@ export default function StatBox(props){
 
 
     return (
-        <div class="stat-box" style={statboxStyle}>
-            <div class="icon-container">
+        <div className="stat-box" style={statboxStyle}>
+            <div className="icon-container">
                 <img src={props.icon} alt="Icon"/>
             </div>
-            <div class="content-container">
-                <p class="total-players">{props.statNumber}</p>
+            <div className="content-container">
+                <p className="total-players">{props.statNumber}</p>
                 <p>{props.statTitle}</p>
-                <p class="percentage-change">{props.statSubtitle}</p>
+                <p className="percentage-change">{props.statSubtitle}</p>
             </div>
         </div>
     )
