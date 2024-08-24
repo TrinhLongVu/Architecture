646522e59b033cdfeb423865e0e00650a135148f
sinful_johnny <megaVNN321123@gmail.com>
2024-08-24 11:42:51 +0700

    500 rows of user data

--- src/services/manage-user/server.js
+++ src/services/manage-user/server.js
@@ -1,11 +1,11 @@
 import app from './src/app.js'
 import db from "./src/models/index.mjs"
-import { importUserSampleData } from './src/models/sampleData/TTNGUOIDUNG.sample.js';
+import { importUserSampleData, createSampleData } from './src/models/sampleData/TTNGUOIDUNG.sample.js';
 
 const PORT = process.env.PORT || 3001;
 
 db.sequelize.sync().then(() => {
-    importUserSampleData();
+    createSampleData();
     app.listen(PORT, () => {
         console.log(`server USER MANAGEMENT is running with PORT ${PORT}`)
     })
--- src/services/manage-user/src/config/config.js
+++ src/services/manage-user/src/config/config.js
@@ -5,7 +5,7 @@ export default {
     "username": "root",
     "password": "userdb",
     "database": "userdb",
-    "host": "userdb",
+    "host": "127.0.0.1",
     "dialect": "mysql",
     "port": "3307"
   },
